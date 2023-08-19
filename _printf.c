3c64734174136f0ac2cebc1d98d85538a3bbc5f5
“rodynaamrfathy” <rodynaamr@icloud.comgit config --global user.email rodynaamr@icloud.com>
2023-08-19 08:24:10 -0700

    ‘hi’

---
+++ _printf.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * printf - a function that produces output according to a format.
+ * @format: strind passed to function.
+ * @...: arguments to be added
+ *
+ *
+ * Return: the number of characters printed (excluding the null byte used to end output to strings)
+ */
+int _printf(const char *format, ...)
+{
+
+}
