df4bcb989c4430e0e99e700c2cd020011ba70cc4
“rodynaamrfathy” <rodynaamr@icloud.comgit config --global user.email rodynaamr@icloud.com>
2023-08-19 10:11:18 -0700

    ‘hi’

--- _printf.c
+++ _printf.c
@@ -1,21 +1,35 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * printf - a function that produces output according to a format.
  * @format: strind passed to function.
  * @...: arguments to be added
  *
- *
  * Return: the number of characters printed (excluding the null byte used to end output to strings)
  */
 int _printf(const char *format, ...)
 {
 	va_list ap;
+	int len = 0;
+	symbols_t symbol_func[] = {{"\%s", }
+				{"\%c", }
+	};
 
+	len = strlen(format);
 	va_start(ap, format);
 
+	/*check if the sting passed "format" contains %s or whatever variable
+	 * pinting special character then call its funcltion with prototype
+	 * will be included in main.h exapmle _sprintf() this function
+	 * will be called when you find %s in the format
+	 * struct for this array will be included in main.h
+	 * len will be increminted with the len of format then the len of
+	 * any other variable to be printed 
+	 * len = strlen(format) + strlen(variables)*/
 
 
-	return ();
+
+	return (len);
 	va_end(ap);
 }
